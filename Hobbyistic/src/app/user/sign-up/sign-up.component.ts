ac419aaa3665a7d228d15dbb9f55d86e38143e1b
sukhbirsekhon <sekhonss@mail.uc.edu>
2022-10-19 13:44:16 -0400

    added validation error messages on sign up form

--- Hobbyistic/src/app/user/sign-up/sign-up.component.ts
+++ Hobbyistic/src/app/user/sign-up/sign-up.component.ts
@@ -9,6 +9,7 @@ import { UserService } from 'src/app/shared/user.service';
   providers: [UserService]
 })
 export class SignUpComponent implements OnInit {
+  emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
   constructor(public userService: UserService) { }
 
