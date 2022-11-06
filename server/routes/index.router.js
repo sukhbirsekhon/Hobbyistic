2a91e30ddedd3e37e81a670121340b2a919aeb76
Jack McHugh <mchughjk@mail.uc.edu>
2022-11-06 17:48:07 -0500

    Updated endpoint authentication & minor bug fixes

--- server/routes/index.router.js
+++ server/routes/index.router.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-
+const auth = require('../routes/auth');
 const userController = require('../controllers/user.controller');
 
 router.post('/register', userController.register);
 router.post('/login', userController.login);
+router.get('/testauth', auth.required, userController.testauth);
 
 module.exports = router;
