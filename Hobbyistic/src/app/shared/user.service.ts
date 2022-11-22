164823e718233dc71bc55d48aa040b02e960d2a3
sukhbirsekhon <sekhonss@mail.uc.edu>
2022-11-21 20:26:23 -0500

    navigate to different page after user is created

--- Hobbyistic/src/app/shared/user.service.ts
+++ Hobbyistic/src/app/shared/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { User } from './user.model';
+import {Router} from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -11,13 +12,14 @@ export class UserService {
     email: '',
     password: ''
   };
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   CreateUser(name: string, email: string, password: string){
     const user: User = {name:name, email: email, password: password}
     this.http.post("http://localhost:3000/api/register", user)
     .subscribe(response =>{
       console.log(response);
+      this.router.navigate(['/login']);
   })
 
   }
