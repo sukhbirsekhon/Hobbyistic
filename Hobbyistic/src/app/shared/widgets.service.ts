b8b3cffd2ca3018b90748437843d4384c46ed84a
sukhbirsekhon <sekhonss@mail.uc.edu>
2023-01-22 17:14:26 -0500

    Added AddTasks impl and corrected GetWidgets impl

--- Hobbyistic/src/app/shared/widgets.service.ts
+++ Hobbyistic/src/app/shared/widgets.service.ts
@@ -13,9 +13,14 @@ export class WidgetsService {
 
   constructor(private http: HttpClient, private router: Router) { }
 
-  GetWidgets(): Observable<Widgets[]> {
+  GetWidgets(hobby: Hobby): Observable<Widgets[]> {
     let header = new HttpHeaders().set('Authorization',`Bearer ${localStorage.getItem('token')!}`);
-    return this.http.get<Widgets[]>("http://localhost:3000/api/hobby", {headers: header});
+    return this.http.get<Widgets[]>("http://localhost:3000/api/hobby/" + hobby.id + "/widgets", {headers: header});
+  }
+
+  AddTasks(tasks: Task, hobby: Hobby): Observable<Task> {
+    let header = new HttpHeaders().set('Authorization',`Bearer ${localStorage.getItem('token')!}`);
+    return this.http.post<Task>("http://localhost:3000/api/hobby/" + hobby.id + "/widgets/tasks", {tasks}, {headers: header});
   }
 
 
