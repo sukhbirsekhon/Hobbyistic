4df8735439b43ea0933c6e7652441c34795d0da8
Nicholas-Lawson <lawsonnj@mail.uc.edu>
2023-02-10 13:41:54 -0500

    Completely Functional Navigational Dropdown Bar
    Using Angular, CSS, HTML, and bootstrap, I created a completely working dropdown. The routes are now fully operational.

--- Hobbyistic/src/app/user/dashboard/motivation/motivation.component.ts
+++ Hobbyistic/src/app/user/dashboard/motivation/motivation.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Hobby } from 'src/app/shared/hobby.model';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from 'src/app/shared/user.service';
 
 @Component({
   selector: 'app-motivation',
   templateUrl: './motivation.component.html',
-  styleUrls: ['./motivation.component.css']
+  styleUrls: ['./motivation.component.css'],
+  providers: [UserService]
 })
 export class MotivationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, public UserService: UserService){ }
+
+  hobby: Hobby = {"name" : ""};
 
   ngOnInit(): void {
+    
+    this.hobby.id = this.route.snapshot.params['id'];
+    this.UserService.GetSingleHobby(this.hobby).subscribe(resp => {
+      this.hobby = resp;
+    });
   }
 
 }
