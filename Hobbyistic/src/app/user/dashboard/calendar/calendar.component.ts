8731e35600ab571e2b91af13b6cfc4186d5df02b
Mahesh Gowda <msg20017@gmail.com>
2023-02-25 22:35:34 -0500

    Calendar UI and started work on events

--- Hobbyistic/src/app/user/dashboard/calendar/calendar.component.ts
+++ Hobbyistic/src/app/user/dashboard/calendar/calendar.component.ts
@@ -3,9 +3,9 @@ import { Hobby } from 'src/app/shared/hobby.model';
 import { ActivatedRoute } from '@angular/router';
 import { UserService } from 'src/app/shared/user.service';
 
-
-import { CalendarOptions } from '@fullcalendar/core'; // useful for typechecking
+import { CalendarOptions, DateSelectArg, EventClickArg, EventApi } from '@fullcalendar/core'; // useful for typechecking
 import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
 
 @Component({
   selector: 'app-calendar',
@@ -28,8 +28,50 @@ export class CalendarComponent implements OnInit {
   }
 
   calendarOptions: CalendarOptions = {
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek'
+    },
     initialView: 'dayGridMonth',
-    plugins: [dayGridPlugin]
+    contentHeight: 'auto',
+    events: [
+      { title: 'event 1', date: '2023-04-01' },
+      { title: 'event 2', date: '2023-04-02' }
+    ],
+    weekends: true,
+    editable: true,
+    selectable: true,
+    selectMirror: true,
+    dayMaxEvents: true,
+    plugins: [dayGridPlugin],
+    select: this.handleDateSelect.bind(this),
+    eventClick: this.handleEventClick.bind(this),
+    //eventsSet: this.handleEvents.bind(this)
+    //eventadd:
   };
 
+  handleDateSelect(selectInfo: DateSelectArg) {
+    const title = prompt('Please enter a new title for your event');
+    const calendarApi = selectInfo.view.calendar;
+
+    calendarApi.unselect(); // clear date selection
+
+    if (title) {
+      calendarApi.addEvent({
+        id: "",
+        title,
+        start: selectInfo.startStr,
+        end: selectInfo.endStr,
+        allDay: selectInfo.allDay
+      });
+    }
+  }
+
+  handleEventClick(clickInfo: EventClickArg) {
+    if (confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
+      clickInfo.event.remove();
+    }
+  }
+    //this.calendarOptions.events = [...this.newEvents];
 }
