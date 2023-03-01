0c6bde225fa18bd8985e4f3d563d704909820abd
Nicholas-Lawson <lawsonnj@mail.uc.edu>
2023-03-01 10:04:51 -0500

    Improved display of widgets (BONUS CONTENT: ADDED WHOLE MODEL INTEGRATION)
    Like and subscribe for more awesome content updates

---
+++ Hobbyistic/src/app/shared/motivationWidget.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { MotivationWidget } from './motivationWidget.model';
+import { Hobby } from './hobby.model';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+    providedIn: 'root'
+  })
+export class motivationWidgetService
+{
+    constructor(private http: HttpClient, private router: Router) { }
+
+    getAllPosts(hobby: Hobby): Observable<MotivationWidget>{
+        let header = new HttpHeaders().set('Authorization',`Bearer ${localStorage.getItem('token')!}`);
+        return this.http.get<MotivationWidget>("http://localhost:3000/api/hobby/" + hobby.id + "/motivationWidgets", {headers: header});
+    }
+
+    addPost(hobby: Hobby, motivationWidget: MotivationWidget): Observable<MotivationWidget>{
+        let header = new HttpHeaders().set('Authorization',`Bearer ${localStorage.getItem('token')!}`);
+        return this.http.post<MotivationWidget>("http://localhost:3000/api/hobby/" + hobby.id + "/motivationWidgets/", motivationWidget, {headers: header});
+    }
+
+    deletePost(hobby: Hobby, motivationWidget: MotivationWidget): Observable<MotivationWidget>{
+        let header = new HttpHeaders().set('Authorization',`Bearer ${localStorage.getItem('token')!}`);
+        return this.http.delete<MotivationWidget>("http://localhost:3000/api/hobby/" + hobby.id + "/motivationWidgets/" + motivationWidget._id, {headers: header});
+    }
+
+}
