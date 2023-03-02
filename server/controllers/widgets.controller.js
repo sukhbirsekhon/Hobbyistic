0e6d110ec9d004a49b49465f82d7a8825b1f4911
Jack McHugh <mchughjk@mail.uc.edu>
2023-03-02 15:51:22 -0500

    get single task

--- server/controllers/widgets.controller.js
+++ server/controllers/widgets.controller.js
@@ -28,6 +28,32 @@ module.exports.getWigets = (req, res, next) => {
     });
 }
 
+module.exports.getSingleTask = (req, res, next) => {
+    if (req.auth == null) {
+        return res.status(401).json({errors: {user: "Unauthorized"}}); 
+    }
+    User.findById(req.auth.id).then(function(user){
+        if (!user) { 
+            return res.status(401).json({errors: {user: "Unauthorized"}}); 
+        }
+        Widgets.findOne(
+            { user: req.auth.id, hobby: req.params.hobbyId, 'taskWidget.tasks': { $elemMatch: { _id: req.params.taskId } } }, 
+            { 'taskWidget.tasks.$': 1 }, 
+            (err, widget) => {
+                if (err) {
+                    return next(err);
+                }
+                if (!widget) {
+                    return res.status(404).json({errors: {widget: "Widget not found"}});
+                }
+                const task = widget.taskWidget.tasks[0];
+                res.send(task);
+            }
+        );
+    });
+}
+
+
 module.exports.getTaskWidget = (req, res, next) => {
     if (req.auth == null) {
         return res.status(401).json({errors: {user: "Unauthorized"}}); 
