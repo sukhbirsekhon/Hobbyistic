797246247f26ab1af21f132b4b297e2668e0e0da
Jack McHugh <mchughjk@mail.uc.edu>
2023-03-02 14:03:39 -0500

    Complete Hobby DL AI

--- server/models/widgets.model.js
+++ server/models/widgets.model.js
@@ -19,6 +19,7 @@ const notesWidgetSchema = new mongoose.Schema({
     note: {type: String, required: true}
 });
 
+
 const calendarWidgetSchema = new mongoose.Schema({
     events: [{
         title: {type: String, required: true},
@@ -56,6 +57,13 @@ const motivationWidgetSchema = new mongoose.Schema({
     metadata: Object
   });
 
+const assistantWidgetSchema = new mongoose.Schema({
+    messages: [{
+        role: {type: String, required: true},
+        content: {type: String, required: true},
+        date: {type: Date, required: true}
+    }]
+})
 
 const widgetsSchema = new mongoose.Schema({
     user: {
@@ -73,6 +81,7 @@ const widgetsSchema = new mongoose.Schema({
     notesWidget: notesWidgetSchema,
     externalLinksWidget: externalLinksWidgetSchema,
     calendarWidget: calendarWidgetSchema,
+    assistantWidget: assistantWidgetSchema
     //motivationWidget: [{type: mongoose.Schema.Types.ObjectId, ref: 'Posts'}]
  });
 
@@ -102,6 +111,12 @@ widgetsSchema.methods.toJSON = function() {
 widgetsSchema.methods.toJSONForTasks = function() {
     return {
         taskWidget: this.taskWidget,
+    };
+};
+
+assistantWidgetSchema.methods.toJSONForAPI = function() {
+    return {
+        messages: this.messages.map(message => ({ role: message.role, content: message.content }))
     };
 };
 
