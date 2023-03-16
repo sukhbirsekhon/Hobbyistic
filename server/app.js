fc0b626e9d8eaf9bc1861805ee87ea8e78dea369
Jack McHugh <mchughjk@mail.uc.edu>
2023-03-16 00:42:15 -0400

    test_session

--- server/app.js
+++ server/app.js
@@ -7,16 +7,33 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 var app = express();
 
 const rtsIndex = require('./routes/index.router');
 
+const store = new MongoDBStore({
+    uri: process.env.MONGODB_URI,
+    collection: 'sessions'
+});
+    
+store.on('error', function(error) {
+    console.log(error);
+});
 // middleware to handle routes
 app.use(bodyParser.json());
 app.use(cors());
 app.use('/api', rtsIndex);
-app.use(session({ secret: process.env.JWT_AUTH_KEY, cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false  }));
+app.use(session({
+    secret: process.env.JWT_AUTH_KEY,
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24
+    }
+  }));
 
 // error handler
 app.use((err, req, res, next) => {
