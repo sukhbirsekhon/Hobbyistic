c8c97b072544f8dbdc90e3dead9b341b86ab5201
Jack McHugh <mchughjk@mail.uc.edu>
2023-03-16 22:52:27 -0400

    make sure newest message is always at the bottom

--- Hobbyistic/src/app/user/dashboard/assistant/assistant.component.ts
+++ Hobbyistic/src/app/user/dashboard/assistant/assistant.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { UserService } from 'src/app/shared/user.service';
 import { Router } from '@angular/router';
 import { Hobby } from 'src/app/shared/hobby.model';
 import { ActivatedRoute } from '@angular/router';
 import { Message } from 'src/app/shared/message.model';
 import { WidgetsService } from 'src/app/shared/widgets.service';
+
 
 @Component({
   selector: 'app-assistant',
@@ -13,13 +14,40 @@ import { WidgetsService } from 'src/app/shared/widgets.service';
   providers: [UserService]
 })
 export class AssistantComponent implements OnInit {
-  
+  @ViewChild('chatBox') chatBox!: ElementRef;
   loading = false;
   hobby: Hobby = {"name" : ""};
   messages: Message[] = [];
   newMessage: Message = {role: 'user', content: '', date: new Date()};
+  
 
   constructor(private UserService: UserService, private WidgetService: WidgetsService, private router: Router, private route: ActivatedRoute) {}
+
+  
+  
+
+  
+  scrollToBottom(): void {
+    if (this.chatBox && this.chatBox.nativeElement) {
+      try {
+        this.chatBox.nativeElement.scrollTo({
+          top: this.chatBox.nativeElement.scrollHeight,
+          behavior: 'smooth'
+        });
+        setTimeout(() => {
+          this.chatBox.nativeElement.scrollTo({
+            top: this.chatBox.nativeElement.scrollHeight,
+            behavior: 'smooth'
+          });
+        }, 500);
+      } catch(err) { 
+        console.log(err);
+      }
+    }
+  }
+
+  
+
 
   ngOnInit(): void {
     this.hobby.id = this.route.snapshot.params['id'];
@@ -28,18 +56,24 @@ export class AssistantComponent implements OnInit {
     });
     this.WidgetService.getAssistantMessageHistory(this.hobby).subscribe(response => {
       this.messages = response;
+      this.scrollToBottom();
     })
+    
   }
+
+  
 
   onSubmit(): void {
     this.loading = true;
     const newMessageContent = this.newMessage.content;
     this.newMessage.content = '';
     this.messages.push({ role: 'user', content: newMessageContent, date: new Date()});
+    this.scrollToBottom();
     this.WidgetService.getAssistantResponseAndMessage(this.hobby, {role: 'user', content: newMessageContent, date: new Date()}).subscribe(response => {
       console.log('rolling')
       this.messages.push({ role: 'assistant', content: response.message, date: new Date()});
       this.loading = false;
+      this.scrollToBottom();
     });
     console.log('woof');
     this.newMessage.content = '';
